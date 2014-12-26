ae908993ba853c7769e8f120c47c92d60d936b04
chris <taubmann@more-elements.com>
2014-12-26 14:18:50 +0100

    fixes

--- backend.php
+++ backend.php
@@ -84,67 +84,6 @@ if (!isset($_SESSION[$projectName])) {
 
     //@unset($_SESSION['TMP__' . $projectName]);
 
-    /**
-     * build HTML for the dropdowns located in the header
-     *
-     * @param $projectName string the project name
-     * @param $objectName string the object name
-     * @param $user_wizards array
-     * @return array
-     */
-    function buildDropdowns($projectName, $objectName, $user_wizards)
-    {
-        $d = array('objectSelect' => '', 'tplSelect' => '', 'uwizSelect' => '');
-        // build Object-Selector
-        $d['objectSelect'] = '<select id="objectSelect">' .
-            '<option value="" data-htype=""> ' . L('availabe_Sections') . " </option>\n";
-
-        foreach ($_SESSION[$projectName]['objectOptions'] as $group => $arr) {
-            $d['objectSelect'] .= '<optgroup label="' . (($group != '0') ? ' ' . $group . '' : '') . '">';
-            foreach ($arr as $option) {
-                $opt_state = ($option['name'] == $objectName) ? ' selected="selected"' : '';
-                if (substr($option['label'], 0, 1) !== '.') {
-                    $d['objectSelect'] .= '	<option' . $opt_state . ' value="' . $option['name'] . '" data-htype="' . $option['htype'] . '"> ' . $option['label'] . '</option>';
-                }
-            }
-            $d['objectSelect'] .= '</optgroup>';
-        }
-        $d['objectSelect'] .= '</select>';
-
-        // build Template-Selector if needed
-        $d['tplSelect'] = '';
-        if ($objectName && count($_SESSION[$projectName]['templates'][$objectName]) > 1) {
-            $d['tplSelect'] .= '<select id="templateSelect">' .
-                '<option value="">' . L('availabe_Templates') . "</option>\n";
-            foreach ($_SESSION[$projectName]['templates'][$objectName] as $templatename) {
-                $d['tplSelect'] .= '<option value="' . $templatename . '">' . L($templatename) . '</option>';
-            }
-            $d['tplSelect'] .= '</select>';
-        }
-
-    // build user wizard selector
-
-        if (isset($user_wizards)) {
-            $d['uwizSelect'] = '
-    <span>
-    <select id="globalWizard" onchange="openGlobalWizard(this)">
-        <option value=""> ' . L('Wizards') . ' </option>
-        <optgroup label="' . L('global_wizards') . '">
-        ';
-            foreach ($user_wizards as $wizards) {
-                if (isset($wizards['url']) && isset($wizards['name'])) {
-                    $d['uwizSelect'] .= '		<option value="' . $wizards['url'] . '"> ' . L($wizards['name']) . ' </option>';
-                }
-            }
-            $d['uwizSelect'] .= '</optgroup>
-        <optgroup id="objectWizards" label="' . L('object_wizards') . '">
-        </optgroup>
-    </select>
-    </span>';
-        }
-        return $d;
-    }
-
 
     $templatePath = $_SESSION[$projectName]['config']['templates'][$template];
 
@@ -153,7 +92,6 @@ if (!isset($_SESSION[$projectName])) {
 
     // load the template
     if (file_exists($templatePath . '/backend.php')) {
-        // load the Template
         include $templatePath . '/backend.php';
     } else {
         exit('template does not exist');
